8c284f67514ddcb575e0457bd842577fa033db8b
Kim Bonnette <kgbonnette@gmail.com>
2020-12-24 03:15:30 -0500

    Added React Suite library of UI components. Changed from generic button to a styled icon button, and added an alert.

--- purple-cow-counter/src/App.js
+++ purple-cow-counter/src/App.js
@@ -1,22 +1,37 @@
+import { Alert, Container, Content, Footer, Header, Icon, IconButton } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
 import './App.css';
 
+// Function that calls the external counting API, gets the latest count, and 
+// displays the new count as an informational alert.
 function callCountApiAndAlert() {
-  console.log("Calling Count Api...")
+  Alert.info("This is a informations.", 4000);
 }
 
+// CountButton React.js component delegates the work of counting and displaying
+// the count to the callCountApiAndAlert() function.
 function CountButton() {
   return (
-    <button onClick={() => callCountApiAndAlert()}>Count!</button>
+    <IconButton icon={<Icon icon="calculator" />} size="lg" color="violet" onClick={() => callCountApiAndAlert()}>Count!</IconButton>
   )
 }
 
+// The main "Purple Cow Counter" app, a React.js component.
 function App() {
   return (
     <div className="App">
-      <h1>Purple Cow Counter</h1>
-      <h2>Main</h2>
-      <CountButton />
-      <h3>Footer</h3>
+      <Container>
+        <Header>
+          <h1>Purple Cow Counter</h1>
+        </Header>
+        <Content>
+          <p>Description here...</p> <br />
+          <CountButton />
+        </Content>
+        <Footer align="bottom">
+          Created by Kim Bonnette for Fearless
+        </Footer>
+      </Container>
     </div>
   );
 }
